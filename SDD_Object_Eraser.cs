ba28788ab2b74cbe0328fde400b78abf4bc3639d
Julio César Ruiz Osorio <93063053+Eldelabici@users.noreply.github.com>
2025-01-10 14:58:08 -0600

    Agregado de un sistema de listas de objetivos a destruir

--- SDD_Object_Eraser.cs
+++ SDD_Object_Eraser.cs
@@ -1,29 +1,21 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
-using System.Linq;
-using Unity.Mathematics;
-using Unity.VisualScripting;
 using UnityEngine;
 
 public class SDD_Object_Eraser : MonoBehaviour
 {
-    public GameObject objetive_Object;
-    public Texture2D objetive_Texture;
-    public SpriteRenderer objetive_SpriteRenderer;
-    public Vector2 objetive_position, destructor_position;
-    public Vector2 localErasePosition;
+    private List<GameObject> objetive_Objects = new List<GameObject>();
+    private List<Texture2D> objetive_Textures = new List<Texture2D>();
+    private List<SpriteRenderer> objetive_SpriteRenderers = new List<SpriteRenderer>();
 
-    public List<Vector2> corrected_World_Positions;
+    private Vector2 localErasePosition;
 
-    public bool margin_corrector;
+    private bool margin_corrector;
 
-    //Margenes de coordenadas para los redondeos
-    public float y_margin, x_margin;
+    // Márgenes de coordenadas para los redondeos
+    private float y_margin, x_margin;
 
-    /*
-    *      Asuminedo que tenemos un collider trigger
-    */
     private void Start()
     {
         y_margin = 0.5f;
@@ -31,65 +23,74 @@ public class SDD_Object_Eraser : MonoBehaviour
 
         margin_corrector = false;
     }
+
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        /*
-         * Recordatorio de determinar si se posee una textura legible
-         *  o preferiblemente una comprobación de script de los distintos destroyables
-         * */
-        //destructor_position = gameObject.transform.position;
-        //recordatorio de hacer comprobaciones por tag o por script aqui
-        objetive_Object = collision.gameObject;
+        GameObject newObject = collision.gameObject;
+        SpriteRenderer newSpriteRenderer = newObject.GetComponent<SpriteRenderer>();
 
-        objetive_SpriteRenderer = objetive_Object.GetComponent<SpriteRenderer>();
-        objetive_Texture = objetive_SpriteRenderer.sprite.texture;
+        if (newSpriteRenderer != null)
+        {
+            Texture2D newTexture = newSpriteRenderer.sprite.texture;
 
+            // Agrega el objeto, sprite y textura a las listas
+            objetive_Objects.Add(newObject);
+            objetive_SpriteRenderers.Add(newSpriteRenderer);
+            objetive_Textures.Add(newTexture);
+        }
     }
+
     private void OnTriggerStay2D(Collider2D collision)
     {
         Position_Based_Erasing(gameObject.transform.position);
     }
+
     private void Position_Based_Erasing(Vector2 erase_Position)
     {
-        // Convierte la posición mundial a local
-        Vector2 localPosition = objetive_Object.transform.InverseTransformPoint(erase_Position);
-        Rect spriteRect = objetive_SpriteRenderer.sprite.rect;
-        Vector2 pivotAdjustedPos = localPosition + (Vector2)objetive_SpriteRenderer.sprite.bounds.extents;
-        // Calcula coordenadas en la textura
-        localErasePosition.x = (pivotAdjustedPos.x / objetive_SpriteRenderer.sprite.bounds.size.x) * spriteRect.width;
-        localErasePosition.y = (pivotAdjustedPos.y / objetive_SpriteRenderer.sprite.bounds.size.y) * spriteRect.height;
-        Vector2Int roundedPosition_local_position = World_to_local_position_Rounding(localErasePosition);
-        // Corrige márgenes y obtiene posiciones adicionales
-        List<Vector2> correctedPositions = new List<Vector2>();
-            
-        correctedPositions.Add(localErasePosition);
+        for (int i = 0; i < objetive_Objects.Count; i++)
+        {
+            GameObject obj = objetive_Objects[i];
+            SpriteRenderer spriteRenderer = objetive_SpriteRenderers[i];
+            Texture2D texture = objetive_Textures[i];
 
-        if (margin_corrector)
-        {
-            correctedPositions.AddRange(PixelCorrector_decimalmargin(localErasePosition));
-                
-        }
-        foreach (Vector2 pos in correctedPositions)
-        {
-            Vector2Int correctedPos = World_to_local_position_Rounding(pos);
-            if (correctedPos.x >= 0 && correctedPos.x < objetive_Texture.width &&
-                correctedPos.y >= 0 && correctedPos.y < objetive_Texture.height)
+            // Convierte la posición mundial a local
+            Vector2 localPosition = obj.transform.InverseTransformPoint(erase_Position);
+            Rect spriteRect = spriteRenderer.sprite.rect;
+            Vector2 pivotAdjustedPos = localPosition + (Vector2)spriteRenderer.sprite.bounds.extents;
+
+            // Calcula coordenadas en la textura
+            localErasePosition.x = (pivotAdjustedPos.x / spriteRenderer.sprite.bounds.size.x) * spriteRect.width;
+            localErasePosition.y = (pivotAdjustedPos.y / spriteRenderer.sprite.bounds.size.y) * spriteRect.height;
+
+            // Corrige márgenes y obtiene posiciones adicionales
+            List<Vector2> correctedPositions = new List<Vector2>();
+            correctedPositions.Add(localErasePosition);
+
+            if (margin_corrector)
             {
-                objetive_Texture.SetPixel(correctedPos.x, correctedPos.y, new Color(0, 0, 0, 0));
+                correctedPositions.AddRange(PixelCorrector_decimalmargin(localErasePosition));
             }
+
+            foreach (Vector2 pos in correctedPositions)
+            {
+                Vector2Int correctedPos = World_to_local_position_Rounding(pos);
+                if (correctedPos.x >= 0 && correctedPos.x < texture.width &&
+                    correctedPos.y >= 0 && correctedPos.y < texture.height)
+                {
+                    texture.SetPixel(correctedPos.x, correctedPos.y, new Color(0, 0, 0, 0));
+                }
+            }
+
+            texture.Apply();
         }
-        objetive_Texture.Apply();
-        correctedPositions.Clear();
     }
 
     private List<Vector2> PixelCorrector_decimalmargin(Vector2 local_position)
     {
         List<Vector2> corrected_positions = new List<Vector2>();
 
-        float decimal_x, decimal_y;
-
-        decimal_x = local_position.x - math.trunc(local_position.x);
-        decimal_y = local_position.y - math.trunc(local_position.y);
+        float decimal_x = local_position.x - Mathf.Floor(local_position.x);
+        float decimal_y = local_position.y - Mathf.Floor(local_position.y);
 
         int new_corrected_x, new_corrected_y;
 
